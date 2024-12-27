5d377e04f8113ff838b47595a065cf8c9c214eff
vitorcalvi <vitorcalvi@vitorcalvis-MacBook-Pro.local>
2024-12-27 09:44:32 +0100

    plane.dyagnosys.com

--- nginx/env.sh
+++ nginx/env.sh
@@ -3,5 +3,9 @@
 export dollar="$"
 export http_upgrade="http_upgrade"
 export scheme="scheme"
+
+# Substitute variables into the Nginx config template
 envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# Start Nginx in the foreground
 exec nginx -g 'daemon off;'
